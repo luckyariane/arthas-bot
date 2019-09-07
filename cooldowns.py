56434b7a88c979240ef6ce9775122b388ca45964
Darlene Hibbs <darlenehibbs@gmail.com>
2019-09-07 15:56:38 -0400

    adding in utility changes from currency branch

---
+++ cooldowns.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+
+one_min = 60
+two_mins = 120
+three_mins = 180
+five_mins = 300
+
+def on_cooldown(timestamp, interval):
+    d = datetime.now() - timestamp
+    if d.seconds < interval: return True
+    return False
+
+def get_cooldown(timestamp, interval):
+    d = datetime.now() - timestamp
+    return d.seconds
+
+def set_cooldown():
+    return datetime.now()
+
+def init_cooldown():
+    return datetime.now() - timedelta(seconds=50000)
+
+
+# other datetime functions
+def convert_timestamp(timestamp, fmt='%Y-%m-%dT%H:%M:%SZ'):
+    return datetime.strptime(timestamp, fmt)
+
+def get_timestamp():
+    return datetime.now()
--- utils.py
+++ utils.py
@@ -1,15 +1,22 @@
-import datetime
+import urllib2, json
+from settings import CLIENT_ID, OAUTH, HELIX_OAUTH
 
 def add_points(instance, user, change_points):
-    r = instance.cur.execute('UPDATE CurrencyUser SET Points = Points + %s WHERE Name in ("%s")' % (change_points, user))
+    r = instance.cur.execute('UPDATE currency SET amount = amount + ? WHERE user = ?', (change_points, user))
+    return success(r.rowcount) 
+    
+def add_points_multi(instance, user_list, change_points, time_tracking=False):
+    r = instance.cur.execute('UPDATE currency SET amount = amount + ? WHERE user in ({seq})'.format(seq=','.join(['?'] * len(user_list))), tuple([change_points] + user_list))
+    if success(r.rowcount) and time_tracking:
+        r = instance.cur.execute('UPDATE currency SET time_increments = time_increments + 1 WHERE user in ({seq})'.format(seq=','.join(['?'] * len(user_list))), tuple(user_list))
     return success(r.rowcount) 
 
 def sub_points(instance, user, change_points):
-    r = instance.cur.execute('UPDATE CurrencyUser SET Points = Points - %s WHERE Name in ("%s")' % (change_points, user))
+    r = instance.cur.execute('UPDATE currency SET amount = amount - ? WHERE user = ?', (change_points, user))
     return success(r.rowcount)
 
 def get_points(instance, user):
-    instance.cur.execute('SELECT Points FROM CurrencyUser WHERE Name = "%s"' % user)
+    instance.cur.execute('SELECT amount FROM currency WHERE user = ?', (user,))
     return int(instance.cur.fetchone()[0])
 
 def success(value):
@@ -33,7 +40,13 @@ def append_file(file_name, data):
     with open(file_name, 'a') as f:
         f.write(str(data))
 
-#def cooldown(timer, time, user=None):
-#    d = datetime.now() - timer
-#    if d.seconds < time:
-        
+def get_webpage(url, h=None):
+    if h:
+        request = urllib2.Request(url, headers=h)
+    else: 
+        request = url
+    return urllib2.urlopen(request).read()
+  
+def convert_json(data):
+    return json.loads(data)
+
