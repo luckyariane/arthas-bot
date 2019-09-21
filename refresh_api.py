ffc4ad8b7fe5cbdf7c05756e1734cc5fb27e8a57
Darlene Hibbs <darlenehibbs@gmail.com>
2019-09-21 14:16:16 -0400

    removed testing commented lines

--- refresh_api.py
+++ refresh_api.py
@@ -28,9 +28,9 @@ def TokenRefreshTime(path):
 
 def RefreshToken(path, refresh_token):
     print 'Refreshing token...'
-    # url = 'https://twitchtokengenerator.com/api/refresh/' + refresh_token
-    # request = urllib2.Request(url)
-    # data = json.loads(urllib2.urlopen(request).read())
+    url = 'https://twitchtokengenerator.com/api/refresh/' + refresh_token
+    request = urllib2.Request(url)
+    data = json.loads(urllib2.urlopen(request).read())
     if 'token' in data:
         new_token = data['token']
         with open(path + r'\arthas-bot\local_settings.py', 'r+') as f:
